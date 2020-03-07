70462fad105d3823cad90aa7c1a2039606f64bd9
Pablo Menz <p.menz.r@gmail.com>
2020-03-07 03:13:55 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -10,7 +10,7 @@ def speak_to_grandma(x)
         elsif !x.==x.upcase
           return "HUH?! SPEAK UP, SONNY!"
         else
-          return "NO, NOT SINCE 1983!"
+          return "NO, NOT SINCE 1938!"
         end
       end
 
