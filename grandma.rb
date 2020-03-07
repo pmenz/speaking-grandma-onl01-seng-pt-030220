6cdc021160506ba0678c6710f360b3462c3505eb
Pablo Menz <p.menz.r@gmail.com>
2020-03-07 02:30:30 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 
 def speak_to_grandma(x)
-  if !'X'=="x".upcase
+  if !X==x.upcase
       return "HUH?! SPEAK UP, SONNY!"
       elsif x== "I LOVE YOU GRANDMA!"
         puts "I LOVE YOU TOO PUMPKIN!"
